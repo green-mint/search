b23fb133d49f8ea030b21c7f2903ef942f22389e
moji-joji <makbar.bscs20seecs@seecs.edu.pk>
2022-01-01 22:48:24 +0500

    Did something

--- src/main.cpp
+++ src/main.cpp
@@ -7,30 +7,27 @@
 
 using namespace std;
 
-#define STOPWORDS_FILENAME "../data/stopwords.csv"
-#define METADATA_FILENAME "../data/metadata.csv"
+#define STOPWORDS_FILENAME "data/stop_words.csv"
+#define METADATA_FILENAME "data/metadata.csv"
 
 int main() {
-  cout << "Hashmap-uh!" << endl;
+    cout << "Hashmap-uh!" << endl;
 
-  // Loading the stopwords 
-  unordered_map<string, char> stopWords;
-  populateStopWords(STOPWORDS_FILENAME, stopWords);
+    // Loading the stopwords
+    unordered_map<string, char> stopWords;
+    populateStopWords(STOPWORDS_FILENAME, stopWords);
 
-  std::unordered_map<std::string, char> stopWords;
-  if (stopWords["after"])
-    cout << "Exists" << endl;
-  else
-    cout << "Doesn't exist";
-  populateStopWords(STOPWORDS_FILENAME, stopWords);
-  if (stopWords["after"])
-    cout << "Exists";
-  else
-    cout << "Doesn't exist" << endl;
+    if (stopWords["after"])
+        cout << "Exists" << endl;
+    else
+        cout << "Doesn't exist";
+    populateStopWords(STOPWORDS_FILENAME, stopWords);
+    if (stopWords["after"])
+        cout << "Exists";
+    else
+        cout << "Doesn't exist" << endl;
 
-
-  // Loading the metadata 
-  unordered_map<string, Article> metadata;
-  populateMetadata(METADATA_FILENAME, metadata);
-
+    // Loading the metadata
+    unordered_map<string, Article> metadata;
+    populateMetadata(METADATA_FILENAME, metadata);
 }
