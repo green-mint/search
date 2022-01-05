e0037a0253c95d80280f4d3f01938914daaaab7c
Asadullah Kashif <s.asad18k@gmail.com>
2022-01-05 13:18:05 +0500

    changed the paths

--- parser/file-divider.py
+++ parser/file-divider.py
@@ -1,7 +1,7 @@
 import os
 
-FILE_PATH = "/mnt/c/Users/saadl/Downloads/another/inverted-index-cleaned.csv/inverted-index-cleaned.csv"
-OUTPUT_DIR = "/home/green-mint/dev/dsa_project/search-engine/barrels/"
+FILE_PATH = "/mnt/d/OneDrive/OneDrive - National University of Sciences & Technology/NUST/3 Semester/DSA/Project/search-engine/data/indexing/inverted-index-cleaned.csv"
+OUTPUT_DIR = "/mnt/d/OneDrive/OneDrive - National University of Sciences & Technology/NUST/3 Semester/DSA/Project/search-engine/data/barrels"
 
 curr_line = 0
 curr_wordId = None
@@ -15,22 +15,22 @@ with open(FILE_PATH, "r") as f:
         if curr_line == 0:
             curr_line += 1
             continue
-        
+
         wordId = line.split(",")[0]
 
         if (wordId != curr_wordId):
             if file is not None:
                 # print("Closing a file...")
                 file.close()
-            
+
             curr_wordId = wordId
             file = open(os.path.join(OUTPUT_DIR, curr_wordId + ".csv"), "w")
             file.write("wordId, fileId, frequency, positions\n")
             file.write(line)
-        
+
         else:
             file.write(line)
-    
+
         # i += 1
         # if i == 40000:
         #     break
