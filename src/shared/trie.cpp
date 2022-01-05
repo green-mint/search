9d1f91b206d4292d536bcfbdba1952bda0356fbb
moji-joji <makbar.bscs20seecs@seecs.edu.pk>
2022-01-06 00:02:17 +0500

    HEHE

--- src/shared/trie.cpp
+++ src/shared/trie.cpp
@@ -6,6 +6,7 @@
 
 using namespace std;
 
+// node class
 TrieNode::TrieNode()
 {
     isEndOfWord = false;
@@ -16,6 +17,7 @@ TrieNode::TrieNode()
     }
 }
 
+// destructor
 TrieNode::~TrieNode()
 {
     for (size_t i = 0; i < NUMBER_OF_ALPHABETS; i++)
@@ -26,15 +28,24 @@ TrieNode::~TrieNode()
         }
     }
 }
+// constructor
 Trie::Trie() { root = new TrieNode(); }
 
+// destructor
 Trie::~Trie() { delete root; }
 
-void Trie::insert(string word,int id)
+bool Trie::isEmpty()
 {
+    return root == NULL;
+}
+
+void Trie::insert(string word, int id)
+{
+    // start from root
     TrieNode *curr = root;
     for (size_t i = 0; i < word.length(); i++)
     {
+        // calculating ascii index
         int index = word[i] - ' ';
         if (curr->children[index] == NULL)
         {
@@ -48,25 +59,30 @@ void Trie::insert(string word,int id)
 
 int Trie::search(string word)
 {
+    // start from root
     TrieNode *curr = root;
     for (size_t i = 0; i < word.length(); i++)
     {
+        // calculating index
         int index = word[i] - ' ';
         if (curr->children[index] == NULL)
-        {   
-
+        {
+            // if word alphabet not found return 1
             return -1;
         }
+        // go deeper
         curr = curr->children[index];
     }
-    if(curr->isEndOfWord)
-    return curr->id;
-    else 
-    return -1;
+    // if it is an acutal word, return the title id we stored
+    if (curr->isEndOfWord)
+        return curr->id;
+    else
+        return -1;
 }
 
 bool Trie::deleteWord(string word)
 {
+    // traverse
     TrieNode *currNode = root;
     for (size_t i = 0; i < word.length(); i++)
     {
@@ -80,13 +96,14 @@ bool Trie::deleteWord(string word)
             return false;
         }
     }
-
+    // just set flag to false
     currNode->isEndOfWord = false;
     return true;
 }
 
 bool Trie::isLeafNode(TrieNode *node)
 {
+    // check if it has atleast one leaf node
     for (size_t i = 0; i < NUMBER_OF_ALPHABETS; i++)
     {
         if (node->children[i] != NULL)
@@ -101,14 +118,17 @@ void Trie::prefixSearchHelper(TrieNode *root, string prefix)
 {
     if (root->isEndOfWord)
     {
+        // print if the root was end of word
         cout << prefix << endl;
     }
 
+    // base case
     if (isLeafNode(root))
         return;
 
     for (int i = 0; i < NUMBER_OF_ALPHABETS; i++)
     {
+        // call recursicely for child i and append next alphabet of child.
         if (root->children[i])
         {
             prefixSearchHelper(root->children[i], prefix + (char)(i + ' '));
@@ -116,21 +136,24 @@ void Trie::prefixSearchHelper(TrieNode *root, string prefix)
     }
 }
 
+// get all prefix matching strings
 void Trie::prefixSearch(TrieNode *root, const string prefix)
 {
     {
         TrieNode *currentNode = root;
 
+        // traverse depth first
         for (u_int32_t i = 0; i < prefix.length(); i++)
         {
             int index = prefix[i] - ' ';
 
+            // if not child, retunr
             if (!currentNode->children[index])
                 return;
 
             currentNode = currentNode->children[index];
         }
-
+        // if a word is found, print
         if (currentNode->isEndOfWord && isLeafNode(currentNode))
         {
             cout << prefix << endl;
@@ -138,6 +161,7 @@ void Trie::prefixSearch(TrieNode *root, const string prefix)
         }
         else if (!isLeafNode(currentNode))
         {
+            // current node is made root.
             prefixSearchHelper(currentNode, prefix);
             return;
         }
